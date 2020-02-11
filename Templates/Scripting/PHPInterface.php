bbcc8b283b54a0ec2471230e4259a00d6a48f8be
Sami Holck <sami.holck@gmail.com>
2020-02-11 15:14:45 +0200

    Initial PHP templates

---
+++ Templates/Scripting/PHPInterface.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * SPHPlayground Framework (http://playgound.samiholck.com/)
+ *
+ * @link      https://github.com/samhol/SPHP-framework for the source repository
+ * @copyright Copyright (c) 2007-${date?date?string("yyyy")} ${user}
+ * @license   https://opensource.org/licenses/MIT The MIT License
+ */
+
+<#if namespace?? && namespace?length &gt; 0>
+namespace ${namespace};
+</#if>
+
+/**
+ * Interface ${name}
+ *
+ * @author  ${user}
+ * @license https://opensource.org/licenses/MIT MIT License
+ * @link    https://github.com/samhol/SPHP-framework Github repository
+ * @filesource
+ */
+interface ${name} {
+  
+}
